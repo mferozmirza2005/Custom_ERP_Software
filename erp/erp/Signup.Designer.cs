05cff287a000b84e5c8bcf73af6ca901a344fc04
Mabid2803 <128504844+Mabid2803@users.noreply.github.com>
2023-12-16 09:15:52 +0500

    check this
    add table logo and set icons

---
+++ erp/erp/Signup.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace erp
+{
+    partial class Signup
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.signUpPage = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.username = new System.Windows.Forms.Label();
+            this.LogIn = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.signUpPage);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.username);
+            this.panel1.Controls.Add(this.LogIn);
+            this.panel1.Location = new System.Drawing.Point(228, 103);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(332, 243);
+            this.panel1.TabIndex = 1;
+            // 
+            // signUpPage
+            // 
+            this.signUpPage.BackColor = System.Drawing.Color.Maroon;
+            this.signUpPage.FlatAppearance.BorderSize = 0;
+            this.signUpPage.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.signUpPage.ForeColor = System.Drawing.Color.White;
+            this.signUpPage.Location = new System.Drawing.Point(226, 165);
+            this.signUpPage.Name = "signUpPage";
+            this.signUpPage.Size = new System.Drawing.Size(75, 23);
+            this.signUpPage.TabIndex = 5;
+            this.signUpPage.Text = "SignUp";
+            this.signUpPage.UseVisualStyleBackColor = false;
+            this.signUpPage.Click += new System.EventHandler(this.signUpPage_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(94, 134);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(207, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(18, 134);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Password:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(94, 96);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(207, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // username
+            // 
+            this.username.AutoSize = true;
+            this.username.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.username.Location = new System.Drawing.Point(18, 96);
+            this.username.Name = "username";
+            this.username.Size = new System.Drawing.Size(70, 16);
+            this.username.TabIndex = 1;
+            this.username.Text = "Username:";
+            // 
+            // LogIn
+            // 
+            this.LogIn.AutoSize = true;
+            this.LogIn.Font = new System.Drawing.Font("Arial", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LogIn.Location = new System.Drawing.Point(15, 43);
+            this.LogIn.Name = "LogIn";
+            this.LogIn.Size = new System.Drawing.Size(117, 34);
+            this.LogIn.TabIndex = 0;
+            this.LogIn.Text = "SignUp";
+            // 
+            // Signup
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Name = "Signup";
+            this.Text = "Form3";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button signUpPage;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label username;
+        private System.Windows.Forms.Label LogIn;
+    }
+}
